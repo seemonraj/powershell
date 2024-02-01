0d0fba08223009c84a191c5172a5581170a6d196
Seemonraj S <seemon.s@outlook.com>
2024-02-01 10:48:43 +0100

    Added Install Scripts

---
+++ Install-AdobeReader.ps1
@@ -0,0 +1,12 @@
+﻿#####################################
+#Install Adobe Reader using Winget
+#####################################
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+if ($ResolveWingetPath){
+$WingetPath = $ResolveWingetPath[-1].Path
+}
+
+$config
+Set-Location $wingetpath 
+.\winget.exe install --exact --id Adobe.Acrobat.Reader.64-bit --silent --accept-package-agreements --accept-source-agreements --scope machine
